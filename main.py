4e40bd38242042952632eef7d05e2f0ba32900a7
siri <dolab.founder@gmail.com>
2018-11-15 01:01:31 +0700

    upload files

---
+++ main.py
@@ -0,0 +1,10 @@
+def func_SUM(A, B):
+    sum_values = A + B
+    return sum_values
+
+if __name__ == '__main__':
+    # initialization
+    A = 2
+    B = 3
+    result = func_SUM(A, B)
+    print("sum:{}".format(result))
